645a14afd15f0015fb53e2ac5e905a2e85ed144b
Elin Waage <elwaage@gmail.com>
2026-01-24 10:09:36 +0100

    testing a tutorial

--- server.mjs
+++ server.mjs
@@ -1,2 +1,21 @@
 import express from "express";
 const app = express()
+
+app.get ('/', (req, res,) =>  {
+    res.send('Home Page')
+})
+
+app.get ('Users', auth, (req, res) => {
+    res.send('Users Page')
+})
+
+function auth (req, res) {
+    if (req.query.admin === 'true') {
+        req.admin = true
+        next()
+    } else {
+        res.send ('No auth')
+    }
+   
+}
+app.listen (3000)
