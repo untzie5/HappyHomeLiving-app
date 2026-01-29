4f9fe0bb34fc095399ebc21babe6783962f46f86
Elin Waage <elwaage@gmail.com>
2026-01-29 15:43:21 +0100

    added usersAPI to server

--- server.mjs
+++ server.mjs
@@ -1,10 +1,12 @@
 import app from "./app/app.mjs";
-import contentRouter from "./Routes/contentAPI.mjs"
+import contentRouter from "./Routes/contentAPI.mjs";
+import UserRouter from "./Routes/UsersAPI.mjs";
 
 
 const PORT = 3000;
 
 app.use("/content", contentRouter);
+app.use("/user", UserRouter);
 
 
 app.listen (PORT, () => {
