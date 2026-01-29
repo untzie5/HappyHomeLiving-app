733f55aaa570a503c833ef204e0c9c216428d7fe
Elin Waage <elwaage@gmail.com>
2026-01-29 11:44:56 +0100

    users in API

--- Routes/UsersAPI.mjs
+++ Routes/UsersAPI.mjs
@@ -1,5 +1,18 @@
 import express from "express"
+import user from "../dataObjects/users.mjs";
+import { generateID } from "../dataObjects/users.mjs";
 
-
+const userRouter = express.Router();
 
 userRouter.use(express.json());
+
+userRouter.post("/", (req, res, next) => {
+
+    let newUser = user();
+    newUser.id = generateId();
+
+
+    res.json(newUser);
+});
+
+export default userRouter;
