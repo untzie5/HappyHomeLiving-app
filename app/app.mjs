58d6f821bba98550385cff276f547ac37359c0b3
Elin Waage <elwaage@gmail.com>
2026-01-29 16:29:15 +0100

    smal changes

--- app/app.mjs
+++ app/app.mjs
@@ -1,6 +1,6 @@
 import express from 'express';
 const app = express();
-app.use(express.json());
+
 
 app.get("/heatlh", (req, red) => resizeBy.json({ ok: true }));
 
--- server.mjs
+++ server.mjs
@@ -10,5 +10,5 @@ app.use("/user", UserRouter);
 
 
 app.listen (PORT, () => {
-    console.log ('Server is running on port ${PORT}')
+console.log (`Server is running on port ${PORT}`)
 });
