4da07049e3502ea6c6ac8188b6800c083ba86d1d
Elin Waage <elwaage@gmail.com>
2026-03-04 11:50:59 +0100

    fixed minor error

--- server.mjs
+++ server.mjs
@@ -6,6 +6,8 @@ const app =  express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+app.use(express.static("public"));
 app.use("/localization", express.static("localization"));
 
 app.get("/api/health", (req, res) => res.json({ ok: true }));
