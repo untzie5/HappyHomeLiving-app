2fc3c7752c5ee83401773f36bb8639f28a8a5e77
Elin Waage <elwaage@gmail.com>
2026-03-26 20:44:28 +0100

    updated the login page to be compatible with i18n

--- public/views/login-view/login.mjs
+++ public/views/login-view/login.mjs
@@ -1,11 +1,20 @@
 import { apiRequest } from "/components/api.mjs";
 import { saveUser } from "/components/session.mjs";
+import { t } from "/views/i18n-client.mjs";
 import "/components/create-user.mjs";
 
 export async function mount({ app, navigate }) {
   const form = app.querySelector("#login-form");
   const openBtn = app.querySelector("#open-create-user");
   const createUserEl = app.querySelector("#createUser");
+
+  app.querySelector("#login-app-title").textContent = await t("ui.login.appTitle");
+  app.querySelector("#login-title").textContent = await t("ui.login.title");
+  app.querySelector("#login-username-label").textContent = await t("ui.login.username");
+  app.querySelector("#login-password-label").textContent = await t("ui.login.password");
+  app.querySelector("#login-submit-btn").textContent = await t("ui.login.submit");
+  app.querySelector("#login-create-text").textContent = await t("ui.login.notUserYet");
+  app.querySelector("#open-create-user").textContent = await t("ui.login.createUser");
 
   openBtn?.addEventListener("click", () => createUserEl.open());
 
